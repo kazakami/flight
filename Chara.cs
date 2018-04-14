5e24891d64543c61719ed8cda652105021db2399
kazakami <kazakami@kmc.gr.jp>
2018-04-14 23:47:45 +0900

    描画を試みるテスト

--- Chara.cs
+++ Chara.cs
@@ -1,15 +1,20 @@
 namespace flight
 {
-    class chara : KzkmEngine.Worker
+    public class Chara : KzkmEngine.Worker
     {
+        KzkmEngine.Mesh m = new KzkmEngine.Mesh();
+        public Chara()
+        {
+            System.Console.WriteLine("fuga");
+            m.LoadFromObj("resources/ente_progress_model/ente progress_export.obj");
+        }
         override public void Update()
         {
-
         }
 
         override public void Draw()
         {
-            
+            m.Render();
         }
     }
 }
--- Scenes.cs
+++ Scenes.cs
@@ -2,6 +2,9 @@ namespace flight
 {
     public class DefaultScene : KzkmEngine.Scene
     {
-        
+        public DefaultScene()
+        {
+            workerManager.AddWorker(new Chara());
+        }
     }
 }
