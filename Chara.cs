4d20ff41ba7e05594df2254ce263f7f4d4e7797d
kazakami <kazakami@kmc.gr.jp>
2018-04-14 19:53:36 +0900

    Workerを継承した自機クラス

---
+++ Chara.cs
@@ -0,0 +1,15 @@
+namespace flight
+{
+    class chara : KzkmEngine.Worker
+    {
+        override public void Update()
+        {
+
+        }
+
+        override public void Draw()
+        {
+            
+        }
+    }
+}
