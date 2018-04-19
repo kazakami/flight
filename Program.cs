8af287146ca5821dd8f1d3e3fae4d00f7e8dccd7
kazakami <kazakami@kmc.gr.jp>
2018-04-20 00:22:44 +0900

    KzkmEngine.Gaeme.Startを用いるようにした

--- Program.cs
+++ Program.cs
@@ -4,13 +4,7 @@
     {   
         static int Main(string[] args)
         {
-            //var m = new KzkmEngine.Mesh();
-            //m.LoadFromObj("resources/ente_progress_model/ente progress_export.obj");
-            //return 0;
-            using (var window = new KzkmEngine.Game(800, 600, "window name", "default", new DefaultScene()))
-            {
-                window.Run(60.0);
-            }
+            KzkmEngine.Game.Start("default", new DefaultScene());
             return 0;
         }
     }
